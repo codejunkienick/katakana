61ee8f060458887ba915e698af905388ce1ec1ae
Nikita Poteryaev <psychedelicespresso@gmail.com>
2016-05-20 19:47:40 +0500

    Start first blogpost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -11,42 +11,20 @@ export default class Home extends Component {
       <div className={styles.home}>
         <Helmet title="Блог"/>
         <BlogEntry
-          title="News example title"
+          title="KATAKANA --version 0.1"
           author="codejunkienick"
           authorLink="http://codejunkienick.github.io"
           date="16-05-2016 09:22">
           <p style={{fontStyle: 'italic'}}>
-            Consectetur nesciunt nobis dolorem placeat maxime quod atque temporibus, numquam perspiciatis impedit amet voluptate fuga esse pariatur atque assumenda voluptatem sed aspernatur sequi possimus quis. Ex suscipit iure placeat officiis!
+            "У тебя есть портфолио?", "Когда вы уже катакану запустите", "Тебе с твоими скиллами на работу пора" - people 
           </p>
           <p>
-            Dolor at et voluptatem non eum earum possimus ut error molestias. Temporibus odit vel vel excepturi sapiente. Quas rerum cupiditate obcaecati numquam eaque possimus. Odit laborum rerum officia qui enim.
+            ...
           </p>
-          <p>
-            Dolor alias provident commodi aliquam nemo. Voluptatibus at atque mollitia a ullam? Cupiditate consectetur facilis voluptatibus libero recusandae commodi quisquam eveniet fuga ratione? Excepturi illo incidunt sunt esse blanditiis 
-Consectetur dolorem voluptatibus velit consequuntur architecto, aperiam. Repudiandae ea illo quibusdam explicabo dolore saepe labore inventore repellendus? Deserunt eos maiores aut quasi sed cumque. Doloremque quasi harum optio odio voluptatum?
-          </p>
-          <p>
-            Consectetur nesciunt nobis dolorem placeat maxime quod atque temporibus, numquam perspiciatis impedit amet voluptate fuga esse pariatur atque assumenda voluptatem sed aspernatur sequi possimus quis. Ex suscipit iure placeat officiis!
-          </p>
-        </BlogEntry>
-        <BlogEntry
-          title="News example title"
-          author="codejunkienick"
-          authorLink="http://codejunkienick.github.io"
-          date="16-05-2016 09:22">
-          <p>
-            Consectetur nesciunt nobis dolorem placeat maxime quod atque temporibus, numquam perspiciatis impedit amet voluptate fuga esse pariatur atque assumenda voluptatem sed aspernatur sequi possimus quis. Ex suscipit iure placeat officiis!
-          </p>
-          <p>
-            Dolor at et voluptatem non eum earum possimus ut error molestias. Temporibus odit vel vel excepturi sapiente. Quas rerum cupiditate obcaecati numquam eaque possimus. Odit laborum rerum officia qui enim.
-          </p>
-          <p>
-            Dolor alias provident commodi aliquam nemo. Voluptatibus at atque mollitia a ullam? Cupiditate consectetur facilis voluptatibus libero recusandae commodi quisquam eveniet fuga ratione? Excepturi illo incidunt sunt esse blanditiis 
-Consectetur dolorem voluptatibus velit consequuntur architecto, aperiam. Repudiandae ea illo quibusdam explicabo dolore saepe labore inventore repellendus? Deserunt eos maiores aut quasi sed cumque. Doloremque quasi harum optio odio voluptatum?
-          </p>
-          <p>
-            Consectetur nesciunt nobis dolorem placeat maxime quod atque temporibus, numquam perspiciatis impedit amet voluptate fuga esse pariatur atque assumenda voluptatem sed aspernatur sequi possimus quis. Ex suscipit iure placeat officiis!
-          </p>
+          <h5>changelog:</h5>
+          <ul>
+            <li>Созданы все страницы сайта</li>
+          </ul>
         </BlogEntry>
       </div>
     );
