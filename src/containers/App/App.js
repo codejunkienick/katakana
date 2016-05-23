6f3577b5dbad18027a8f2f818eb2323f8b6b6a43
Nikita Poteryaev <psychedelicespresso@gmail.com>
2016-05-23 17:08:59 +0500

    svg loader works better

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -111,7 +111,9 @@ class App extends Component {
     }
 
     return (
-        <div className={styles.app}>
+      <div
+        style={{height: '100%'}}
+        className={styles.app}>
           <Helmet {...config.app.head}/>
           <AppBar
             zDepth={0}
@@ -173,86 +175,18 @@ export default class AppWrapper extends Component {
   }
 
   componentDidMount() {
-    this.setState({loading: false});
+    this.timer = setTimeout(
+      () => { this.setState({loading: false}); },
+        500
+    );
   }
-  
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+    
   render() {
-    const style = `
-.sk-folding-cube {
-  margin: 40px auto;
-  width: 40px;
-  height: 40px;
-  position: relative;
-  -webkit-transform: rotateZ(45deg);
-          transform: rotateZ(45deg); }
-  .sk-folding-cube .sk-cube {
-    float: left;
-    width: 50%;
-    height: 50%;
-    position: relative;
-    -webkit-transform: scale(1.1);
-        -ms-transform: scale(1.1);
-            transform: scale(1.1); }
-  .sk-folding-cube .sk-cube:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #333;
-    -webkit-animation: sk-foldCubeAngle 2.4s infinite linear both;
-            animation: sk-foldCubeAngle 2.4s infinite linear both;
-    -webkit-transform-origin: 100% 100%;
-        -ms-transform-origin: 100% 100%;
-            transform-origin: 100% 100%; }
-  .sk-folding-cube .sk-cube2 {
-    -webkit-transform: scale(1.1) rotateZ(90deg);
-            transform: scale(1.1) rotateZ(90deg); }
-  .sk-folding-cube .sk-cube3 {
-    -webkit-transform: scale(1.1) rotateZ(180deg);
-            transform: scale(1.1) rotateZ(180deg); }
-  .sk-folding-cube .sk-cube4 {
-    -webkit-transform: scale(1.1) rotateZ(270deg);
-            transform: scale(1.1) rotateZ(270deg); }
-  .sk-folding-cube .sk-cube2:before {
-    -webkit-animation-delay: 0.3s;
-            animation-delay: 0.3s; }
-  .sk-folding-cube .sk-cube3:before {
-    -webkit-animation-delay: 0.6s;
-            animation-delay: 0.6s; }
-  .sk-folding-cube .sk-cube4:before {
-    -webkit-animation-delay: 0.9s;
-            animation-delay: 0.9s; }
-
-@-webkit-keyframes sk-foldCubeAngle {
-  0%, 10% {
-    -webkit-transform: perspective(140px) rotateX(-180deg);
-            transform: perspective(140px) rotateX(-180deg);
-    opacity: 0; }
-  25%, 75% {
-    -webkit-transform: perspective(140px) rotateX(0deg);
-            transform: perspective(140px) rotateX(0deg);
-    opacity: 1; }
-  90%, 100% {
-    -webkit-transform: perspective(140px) rotateY(180deg);
-            transform: perspective(140px) rotateY(180deg);
-    opacity: 0; } }
-
-@keyframes sk-foldCubeAngle {
-  0%, 10% {
-    -webkit-transform: perspective(140px) rotateX(-180deg);
-            transform: perspective(140px) rotateX(-180deg);
-    opacity: 0; }
-  25%, 75% {
-    -webkit-transform: perspective(140px) rotateX(0deg);
-            transform: perspective(140px) rotateX(0deg);
-    opacity: 1; }
-  90%, 100% {
-    -webkit-transform: perspective(140px) rotateY(180deg);
-            transform: perspective(140px) rotateY(180deg);
-    opacity: 0; } }
-`
+    const loader = require('./loader.svg');
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
         <div>
@@ -264,18 +198,12 @@ export default class AppWrapper extends Component {
               display: 'flex',
               justifyContent: 'center', 
               alignItems: 'center',
-              flexDirection: 'column'
+              flexDirection: 'column',
+              fontSize: '1.45em',
+              fontFamily: 'Helvetica, sans-serif'
             }}>
-            <style>
-              {style}
-            </style>
-              <div className="sk-folding-cube">
-                <div className="sk-cube1 sk-cube"></div>
-                <div className="sk-cube2 sk-cube"></div>
-                <div className="sk-cube4 sk-cube"></div>
-                <div className="sk-cube3 sk-cube"></div>
-              </div>
-              Ожидайте...
+            <img style={{width: '100px'}}src={loader}/>
+              Ожидайте
             </div>
           }
         </div>
