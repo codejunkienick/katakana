ae9c474dc5832ba3b65a3faaaa38fb9783e34f0c
Nikita Poteryaev <psychedelicespresso@gmail.com>
2016-05-28 19:13:27 +0500

    Add notfound page

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export default function NotFound() {
+  const styles = require('./NotFound.scss');
   return (
     <div className="container">
-      <h1>Doh! 404!</h1>
-      <p>These are <em>not</em> the droids you are looking for!</p>
+      <h1 className={styles.header}>404</h1>
+      <h1 className={styles.subheader}>Страница не найдена</h1>
+      <p className={styles.text}>
+        > rm -rf katakana.xyz <br />
+        > access denied<br />
+        > exit<br />
+      </p>
     </div>
   );
 }
