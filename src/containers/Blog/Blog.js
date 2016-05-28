2c56b9c9ed4338b079d2375decd0c36a6ddce843
Nikita Poteryaev <psychedelicespresso@gmail.com>
2016-05-28 13:41:59 +0500

    Style blogpost links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import config from '../../config';
 import Helmet from 'react-helmet';
-import { BlogEntry } from 'components';
+import { BlogEntry, Spinner} from 'components';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
 @connect(
-  state => ({posts: state.blog.posts}))
+  state => ({
+    posts: state.blog.posts,
+    loading: state.blog.loading
+  }))
 export default class Home extends Component {
   render() {
     const styles = require('./Blog.scss');
     return (
       <div className={styles.blog}>
         <Helmet title="Блог"/>
-        {this.props.posts.map((post, idx) => <BlogEntry {...post} key={idx} />)}
+        {this.props.posts && this.props.posts.map((post, idx) => <BlogEntry {...post} key={idx} />)}
+        {this.props.loading && !this.props.posts && <Spinner />}
       </div>
     );
   }
